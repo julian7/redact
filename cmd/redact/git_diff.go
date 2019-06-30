760c1d60f29b9d6cbfc9784caff4da8bdc97f20e
Balazs Nagy <julsevern@gmail.com>
2019-06-30 21:15:50 +0200

    git diff command
    it still emits a lot of zeroes though

---
+++ cmd/redact/git_diff.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var gitDiffCmd = &cobra.Command{
+	Use:   "diff FILENAME",
+	Short: "Decoding file from arg, to STDOUT",
+	Run:   gitDiffDo,
+}
+
+func init() {
+	gitCmd.AddCommand(gitDiffCmd)
+}
+
+func gitDiffDo(cmd *cobra.Command, args []string) {
+	masterkey, err := basicDo()
+	if err != nil {
+		cmdErrHandler(err)
+		return
+	}
+	if len(args) != 1 {
+		cmdErrHandler(errors.New("redact git diff requires a single argument"))
+		return
+	}
+	reader, err := os.Open(args[0])
+	defer reader.Close()
+	if err != nil {
+		cmdErrHandler(err)
+		return
+	}
+	err = masterkey.Decode(reader, os.Stdout)
+	if err == nil {
+		return
+	}
+	n, err := reader.Seek(0, os.SEEK_SET)
+	if err != nil {
+		cmdErrHandler(errors.Wrap(err, "re-reading file from beginning"))
+	}
+	if n != 0 {
+		cmdErrHandler(errors.Errorf("cannot return to beginning of file: returned to position %d instead", n))
+		return
+	}
+	readbuf := make([]byte, 1024)
+	for {
+		_, err := reader.Read(readbuf)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			cmdErrHandler(errors.Wrap(err, "reading file"))
+			return
+		}
+		os.Stdout.Write(readbuf)
+	}
+
+}
