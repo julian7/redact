50f5ebd0a2fb52c271f915b281e6dc2c0b62f075
Balazs Nagy <julsevern@gmail.com>
2024-10-02 19:35:42 +0200

    redact-ext-aws get and put mixed up

--- cmd/redact-ext-aws/put.go
+++ cmd/redact-ext-aws/put.go
@@ -3,32 +3,46 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/urfave/cli/v3"
 )
 
 func cmdPut() *cli.Command {
 	return &cli.Command{
-		Name:        "get",
-		Usage:       "Gett secret from AWS Param Store",
+		Name:        "put",
+		Usage:       "Put secret to AWS Param Store",
 		ArgsUsage:   "keyid=kms_key_id_or_alias param=/path/to/param",
-		Description: "Prints Param Store secret to STDOUT",
+		Description: "Reads secret from STDIN and writes to Param Store",
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			cfg, err := loadConfig(cmd.Args().Slice())
 			if err != nil {
 				return err
 			}
 
+			key, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				return err
+			}
+			keyStr := string(key)
+
 			ssmClient, err := ssmClient(ctx)
 			if err != nil {
 				return err
 			}
 			getParamRet, err := cfg.get(ctx, ssmClient)
-			if err != nil {
-				return err
+			if err == nil {
+				gotSecret := getParamRet.Parameter.Value
+				if gotSecret != nil && *gotSecret == keyStr {
+					return ErrAlreadyWritten
+				}
 			}
 
-			fmt.Print(*getParamRet.Parameter.Value)
+			_, err = cfg.put(ctx, ssmClient, keyStr)
+			if err != nil {
+				return fmt.Errorf("writing secret %s: %w", cfg.ParamPath, err)
+			}
 
 			return nil
 		},
